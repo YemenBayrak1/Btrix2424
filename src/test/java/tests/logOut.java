67b31d8d25061e604ed547fffc04650ee5ab3eb9
Yemenbayrak1 <ymnbayrak1629@gmail.com>
2021-10-26 18:50:03 +0200

    ikiside commit

--- src/test/java/tests/logOut.java
+++ src/test/java/tests/logOut.java
@@ -2,6 +2,6 @@ package tests;
 
 public class logOut {
     public static void main(String[] args) {
-        System.out.println("Merhaba Dünya2");
+        System.out.println(". Dünya2");
     }
 }
