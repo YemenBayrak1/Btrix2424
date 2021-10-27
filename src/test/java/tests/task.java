06ad31ec8235455ed162ac9f9322d41151746116
Yemenbayrak1 <ymnbayrak1629@gmail.com>
2021-10-27 12:41:42 +0200

    hi metod added

--- src/test/java/tests/task.java
+++ src/test/java/tests/task.java
@@ -1,8 +1,15 @@
 package tests;
 
 public class task {
-  public static void main(String[] args){
-  System.out.println("Hello Word");
+  public static void main(String[] args) {
+    System.out.println("Hello Word");
   }
-   
+  public static void hi(){
+    System.out.println("Hello Word");
+  }
 }
+
+
+
+   
+
